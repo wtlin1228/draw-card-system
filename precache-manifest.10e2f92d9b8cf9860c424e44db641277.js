557d26ab3ebf0192a9fa3fc6232e26525ad2f2a9
wtlin1228 <wtlin1228@gmail.com>
2020-07-16 04:44:13 +0800

    Updates

---
+++ precache-manifest.10e2f92d9b8cf9860c424e44db641277.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89099a17fc642dfe8cc24d75518adc7c",
+    "url": "/draw-card-system/index.html"
+  },
+  {
+    "revision": "448342bbed2045d66a13",
+    "url": "/draw-card-system/static/js/2.05dfd889.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/draw-card-system/static/js/2.05dfd889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce677f3c64a9b3b25886",
+    "url": "/draw-card-system/static/js/main.0bcf6277.chunk.js"
+  },
+  {
+    "revision": "db2191901faa3660c117",
+    "url": "/draw-card-system/static/js/runtime-main.d29177d2.js"
+  }
+]);
